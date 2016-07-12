d287ae7f1a9caff429596d0355896cc0ffaac656
Alfredo Amengual <amengual.alfredo@gmail.com>
2016-07-11 19:03:20 -0500

    hw done

--- js/stargazer.js
+++ js/stargazer.js
@@ -1,5 +1,24 @@
-function search(name) {
-  var api = 'http://www.strudel.org.uk/lookUP/json/?name='+name;
-  // requires "jsonp" datatype.
-  
+ $(document).ready(function(){
+    $('#search').on('submit', function(event) {
+      event.preventDefault();
+      var starSearch = $("#search-name").val();
+      search(starSearch);
+
+    });
+
+});
+
+
+ function search(name) {
+    var api = 'http://www.strudel.org.uk/lookUP/json/?name='+ name;
+    $.ajax({
+      url: api,
+      dataType: 'jsonp',
+      jsonpCallback: "showImage"
+    })
+}
+
+function showImage(response) {
+  var image = response.image.src;
+  $('#preview').html(`<div><img src="${image}" /></div>`)
 }
